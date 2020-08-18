88943e23d0ea9b1a39faacdb3ae9ce1618a32524
Mo Talafuse <62037395+mohindertalafuse@users.noreply.github.com>
2020-08-18 11:07:10 -0500

    Update to remove a bunch of things

--- choosethemusic.py/Choose The Music.py
+++ choosethemusic.py/Choose The Music.py
@@ -17,221 +17,63 @@ def menu():
     print('2 = Meowter Space')
     print('3 = Paws And Play')
     print('4 = Whisker')
-    
-    
-    mixer.music.load('pawsandplay.mp3')
-    mixer.music.load('whisker.mp3')
+    songs = ["mp3d.mp3", "meowterspace.mp3", "pawsandplay.mp3", "whisker.mp3"]
     x = int(input('Song: '))
 
-    if x == 1:
-        mixer.music.load('mp3d.mp3')
-        songplay = str(input('Looped? y/n '))
-        if songplay == 'y':
-            print('Playing')
-            mixer.music.play(-1)
-            pygame.time.wait(30000)
-            mixer.music.pause()
-            print('Choose again? y/n')
-            xf = str(input())
+    
+    mixer.music.load(songs[x])
+    songplay = str(input('Looped? y/n '))
+    if songplay == 'y':
+        print('Playing')
+        mixer.music.play(-1)
+        pygame.time.wait(30000)
+        mixer.music.pause()
+        print('Choose again? y/n')
+        xf = str(input())
 
-            if xf == 'y':
-                rego()
-            elif xf == 'n':
-                print('Continue? y/n')
-                xg = str(input())
-                if xg == 'y':
-                    mixer.music.unpause()
-                    an = mixer.music.get_pos()
-                    pygame.time.wait(an)
-                    print('1 repitition over. Break loop and reset? y/n')
-                    xv = str(input())
-                    if xv == 'y':
-                        rego()
-                elif xg == 'n':
-                   terminate()
+        if xf == 'y':
+            rego()
+        elif xf == 'n':
+            print('Continue? y/n')
+            xg = str(input())
+            if xg == 'y':
+                mixer.music.unpause()
+                an = (mixer.music.get_length() - mixer.music.get_pos * -1)
+                pygame.time.wait(an)
+                print('1 repitition over. Break loop and reset? y/n')
+                xv = str(input())
+                if xv == 'y':
+                    rego()
+            elif xg == 'n':
+               terminate()
 
-            mixer.music.play(-1)
-        elif songplay == 'n':
-            print('Playing')
-            mixer.music.play()
-            pygame.time.wait(30000)
-            mixer.music.pause()
-            print('Choose again? y/n')
-            xf = str(input())
+        mixer.music.play(-1)
+    elif songplay == 'n':
+        print('Playing')
+        mixer.music.play()
+        pygame.time.wait(30000)
+        mixer.music.pause()
+        print('Choose again? y/n')
+        xf = str(input())
 
-            if xf == 'y':
-                rego()
-            elif xf == 'n':
-                print('Continue? y/n')
-                xg = str(input())
-                if xg == 'y':
-                    mixer.music.unpause()
-                    an = mixer.music.get_pos()
-                    pygame.time.wait(an)
-                    print('Song over. Reset? y/n')
-                    xv = str(input())
-                    if xv == 'y':
-                        rego()
-                elif xg == 'n':
-                    terminate()
+        if xf == 'y':
+            rego()
+        elif xf == 'n':
+            print('Continue? y/n')
+            xg = str(input())
+            if xg == 'y':
+                mixer.music.unpause()
+                an = (mixer.music.get_length() - mixer.music.get_pos * -1)
+                pygame.time.wait(an)
+                print('Song over. Reset? y/n')
+                xv = str(input())
+                if xv == 'y':
+                    rego()
+            elif xg == 'n':
+                terminate()
 
-            
-    elif x == 2:
-        mixer.music.load('meowterspace.mp3')
-        songplay1 = str(input('Looped? y/n '))
-        if songplay1 == 'y':
-            print('Playing')
-            mixer.music.play(-1)
-            mixer.music.play(-1)
-            pygame.time.wait(30000)
-            mixer.music.pause()
-            print('Choose again? y/n')
-            xf = str(input())
-
-            if xf == 'y':
-                rego()
-            elif xf == 'n':
-                print('Continue? y/n')
-                xg = str(input())
-                if xg == 'y':
-                    mixer.music.unpause()
-                    an = mixer.music.get_pos()
-                    pygame.time.wait(an)
-                    print('1 repitition over. Break loop and reset? y/n')
-                    xv = str(input())
-                    if xv == 'y':
-                        rego()
-                elif xg == 'n':
-                   terminate()
-
-        elif songplay1 == 'n':
-            print('Playing')
-            mixer.music.play()
-            pygame.time.wait(30000)
-            mixer.music.pause()
-            print('Choose again? y/n')
-            xf = str(input())
-
-            if xf == 'y':
-                rego()
-            elif xf == 'n':
-                print('Continue? y/n')
-                xg = str(input())
-                if xg == 'y':
-                    mixer.music.unpause()
-                    an = mixer.music.get_pos()
-                    pygame.time.wait(an)
-                    print('Song over. Reset? y/n')
-                    xv = str(input())
-                    if xv == 'y':
-                        rego()
-                elif xg == 'n':
-                    terminate()
-
-
-    elif x == 3:
-        mixer.music.load('pawsandplay.mp3')
-        songplay2 = str(input('Looped? y/n '))
-        if songplay2 == 'y':
-            print('Playing')
-            mixer.music.play(-1)
-            pygame.time.wait(30000)
-            mixer.music.pause()
-            print('Choose again? y/n')
-            xf = str(input())
-
-            if xf == 'y':
-                rego()
-            elif xf == 'n':
-                print('Continue? y/n')
-                xg = str(input())
-                if xg == 'y':
-                    mixer.music.unpause()
-                    an = mixer.music.get_pos()
-                    pygame.time.wait(an)
-                    print('1 repitition over. Break loop and reset? y/n')
-                    xv = str(input())
-                    if xv == 'y':
-                        rego()
-                elif xg == 'n':
-                   terminate()
-
-        elif songplay2 == 'n':
-            print('Playing')
-            mixer.music.play()
-            pygame.time.wait(30000)
-            mixer.music.pause()
-            print('Choose again? y/n')
-            xf = str(input())
-
-            if xf == 'y':
-                rego()
-            elif xf == 'n':
-                print('Continue? y/n')
-                xg = str(input())
-                if xg == 'y':
-                    mixer.music.unpause()
-                    an = mixer.music.get_pos()
-                    pygame.time.wait(an)
-                    print('Song over. Reset? y/n')
-                    xv = str(input())
-                    if xv == 'y':
-                        rego()
-                elif xg == 'n':
-                    terminate()
-
-
-    elif x == 4:
-        mixer.music.load('whisker.mp3')
-        songplay3 = str(input('Looped? y/n '))
-        if songplay3 == 'y':
-            print('Playing')
-            mixer.music.play(-1)
-            pygame.time.wait(30000)
-            mixer.music.pause()
-            print('Choose again? y/n')
-            xf = str(input())
-
-            if xf == 'y':
-                rego()
-            elif xf == 'n':
-                print('Continue? y/n')
-                xg = str(input())
-                if xg == 'y':
-                    mixer.music.unpause()
-                    an = mixer.music.get_pos()
-                    pygame.time.wait(an)
-                    print('1 repitition over. Break loop and reset? y/n')
-                    xv = str(input())
-                    if xv == 'y':
-                        rego()
-                elif xg == 'n':
-                   terminate()
-
-        elif songplay3 == 'n':
-            print('Playing')
-            mixer.music.play()
-            pygame.time.wait(30000)
-            mixer.music.pause()
-            print('Choose again? y/n')
-            xf = str(input())
-
-            if xf == 'y':
-                rego()
-            elif xf == 'n':
-                print('Continue? y/n')
-                xg = str(input())
-                if xg == 'y':
-                    mixer.music.unpause()
-                    an = mixer.music.get_pos()
-                    pygame.time.wait(an)
-                    print('Song over. Reset? y/n')
-                    xv = str(input())
-                    if xv == 'y':
-                        rego()
-                elif xg == 'n':
-                    terminate()
-
+    
+        
 
 
 menu()
